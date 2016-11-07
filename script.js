a27adb022f2cf0f8ceacb60520795cfce3430645
Diogo Vianna <diogoviannaaraujo@icloud.com>
2016-11-07 01:43:01 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (function($) {
   var attachMenuBehav = function() {
-    var toggle = document.querySelector('.toggle')
-    toggle.addEventListener('click', function(event) {
-      var hidable = document.querySelector('.hidable')
-      var content = document.querySelector('.page-content')
-      hidable.classList.toggle('hidden')
-      content.classList.toggle('hidden')
+    var toggle = $('.toggle')
+    toggle.click( function(event) {
+      var hidable = $('.hidable')
+      var content = $('.page-content')
+      hidable.toggleClass('hidden')
+      content.toggleClass('hidden')
     })
   }
 
