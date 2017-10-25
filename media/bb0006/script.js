a68a5bd2312e04aea1a67e1027aaf164ac6f5f02
Diogo Vianna V. Araújo <diogovianna@Diogos-MBP.home>
2017-10-24 22:41:39 -0200

    typo

--- media/bb0006/script.js
+++ media/bb0006/script.js
@@ -2,9 +2,10 @@ var promise = document.querySelector('audio').play();
 
 if (promise !== undefined) {
     promise.catch(error => {
+      document.getElementById('playBtn').classList.add("visible");
       document.getElementById('playBtn').onclick(function() {
         document.getElementById('audio').play();
-      }
+      })
     }).then(() => {
 
     });
