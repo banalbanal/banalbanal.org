76afe291de7e534eae77fe33db75af5cf77ad894
Guilherme Gutierres Gallo <guilherme.gg1@hotmail.com>
2020-11-01 16:20:56 -0300

    Init Vitória Cribb.

---
+++ media/bb0010/js/index.js
@@ -0,0 +1,8 @@
+function onToggleAudio() {
+  console.log('oi')
+}
+
+function onToggleText() {
+  var element = document.getElementById("vcb-txts");
+  element.classList.toggle("vcb-txts-show");
+}
